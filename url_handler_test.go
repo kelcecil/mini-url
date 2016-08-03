708ffed2df6e2292955057cd6b1f39fb72673cd6
Kel Cecil <kelcecil@praisechaos.com>
2016-08-02 23:20:48 -0400

    Add test for redirection

--- url_handler_test.go
+++ url_handler_test.go
@@ -3,9 +3,12 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
@@ -41,6 +44,50 @@ func TestAddNewUrlViaHandler(t *testing.T) {
 	if handler.Storage.GetUrlByShortHash(key.Key) != "http://google.com" {
 		t.Errorf("Hash provided in response to URL did not match URL in store. Key: %v, URL: %v", key.Key, newUrl.URL)
 	}
+}
+
+func TestRedirectToStoredUrl(t *testing.T) {
+	redirectError := errors.New("Redirect occurred")
+	storage := MapUrlStorage{
+		Storage: map[string]string{
+			"a": "http://google.com",
+			"b": "http://kelcecil.com",
+		},
+	}
+	testServer := initializeTestServer(&storage)
+	defer testServer.Close()
+
+	client := &http.Client{
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			return redirectError
+		},
+	}
+	for key, value := range storage.Storage {
+		tryGetURL(t, storage, client, key, testServer.URL, value)
+	}
+}
+
+func tryGetURL(t *testing.T, storage MapUrlStorage, client *http.Client, key, redirectUrl, desiredUrl string) {
+	targetUrl := fmt.Sprintf("%v/%v", redirectUrl, key)
+	req, err := http.NewRequest(http.MethodGet, targetUrl, nil)
+	checkForError(t, "Request for get failed", err)
+
+	t.Logf("Fetching target url: %v", targetUrl)
+	response, _ := client.Do(req)
+
+	redirectedUrl, err := response.Location()
+	checkForError(t, "Unable to retrieve response location", err)
+
+	if !strings.Contains(desiredUrl, redirectedUrl.Host) {
+		t.Errorf("The url was expected to be %v but was %v instead.", desiredUrl, redirectedUrl.Host)
+	}
+}
+
+func initializeTestServer(storage ShortUrlStorage) *httptest.Server {
+	handler := ShortUrlForwardingHandler{
+		Storage: storage,
+	}
+	return httptest.NewServer(handler)
 }
 
 func checkForError(t *testing.T, message string, err error) {
